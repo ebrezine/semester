3e9b1cc4612ae8865a50a4ce905ea7f13df013d6
ebrezine <elias.brezine@gmail.com>
2022-04-16 20:00:36 -0700

    adding seeds

--- project/priv/repo/seeds.exs
+++ project/priv/repo/seeds.exs
@@ -9,3 +9,35 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Project.Company.{Status, Area, Expertise}
+alias Project.Repo
+
+Repo.insert_all(
+  Status,
+  [
+    %{name: "To Do", inserted_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second), updated_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)},
+    %{name: "Working", inserted_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second), updated_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)}
+    %{name: "Finished", inserted_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second), updated_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)}  
+  ],
+  on_conflict: :nothing
+)
+
+Repo.insert_all(
+  Area,
+  [
+    %{name: "Front End", inserted_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second), updated_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)},
+    %{name: "Back End", inserted_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second), updated_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)},
+    %{name: "UX", inserted_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second), updated_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)}
+  ],
+  on_conflicts: :nothing
+);
+
+Repo.insert_all(
+  Expertise,
+  [
+    %{name: "Front End", inserted_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second), updated_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)},
+    %{name: "Back End", inserted_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second), updated_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)},
+  ],
+  on_conflicts: :nothing
+);
